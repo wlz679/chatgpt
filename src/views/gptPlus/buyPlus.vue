d1c383ca51a6eae7485e50c4f7099d43e1ae31e9
wanglizhu <wlz199184@gmail.com>
2024-04-12 23:36:28 +0800

    init

--- src/views/gptPlus/buyPlus.vue
+++ src/views/gptPlus/buyPlus.vue
@@ -1,15 +1,475 @@
 <!-- 升级plus -->
 <template>
   <div class="common-container">
-    <h2>ChatGPT Plus 代充、代升级以及成品号购买 – 正规充值，被封可退钱</h2>
+    <h2 class="big-title" style="margin-bottom: 20px">
+      ChatGPT Plus 代充、代升级以及成品号购买 – 正规充值，被封可退钱
+    </h2>
     <div class="content">
-      <div>
-        <section id="section1" style="height: 800px">第一部分的内容...</section>
-        <section id="section2" style="height: 800px">第二部分的内容...</section>
-        <section id="section3" style="height: 800px">第三部分的内容...</section>
+      <div class="main-box">
+        <!-- plus 升级服务介绍 -->
+        <section id="section1">
+          <div class="box">
+            <h3 class="big-title">一、ChatGPT Plus 升级服务介绍</h3>
+            <p class="desc">
+              你好，欢迎来到ChatGPT的世界，
+              @林兄GPT（微信：3056978），我这篇文章会讲明白 ChatGPT Plus
+              到底是怎么样的产品、怎么升级、本站所提供的服务与价格等~
+            </p>
+          </div>
+
+          <div class="box">
+            <h3 id="section1-1" class="title">1.1 为什么买 GPT-4 呢？</h3>
+            <p class="desc">
+              通常大家所说的要用上 GPT-4，是需要从 3.5 升级，ChatGPT 称之为
+              ChatGPT Plus。截止到 2024 年 3 月，我服务了超过 1000 个
+              Plus会员客户，他们买 ChatGPT Plus 会员的动力是这样的：
+            </p>
+
+            <ul class="list">
+              <li>
+                <strong>娱乐</strong>
+                ：在抖音&视频号看到那些博主在介绍GPT-4，语音对讲很好玩啊！AI
+                绘画效果好、简单上手快
+              </li>
+              <li>
+                <strong>工作</strong>：在工作日常已经用上了
+                3.5，这是效率神器啊。想升级一把，让回答的质量更高，还能联网查询
+              </li>
+              <li>
+                <strong>好奇</strong>：大家都说 GPT-4
+                很强，我想试试看，所谓的最强 AI 到底怎么样？
+              </li>
+              <li>
+                <strong>产品引导</strong>：ChatGPT
+                的网页版和客户端，在显眼的位置引导我升级。
+              </li>
+              <li>…… 等等，也许你有你的想法，欢迎找我聊聊哈哈</li>
+            </ul>
+
+            <p class="desc">
+              可是，这些客户网友朋友说：「我试过自己升级，但升级不了啊」。「Your
+              card has been declined」您的卡已被拒绝……
+              各种问题会出现，关于升级的难点本文后半段等下细说。
+            </p>
+            <p class="desc" style="margin-top: 20px">
+              恭喜你找到了这里，<strong
+                >我提供专业Plus 代充服务：10
+                分钟办好，长期稳定服务，售后保障好。</strong
+              >
+            </p>
+          </div>
+
+          <div class="box">
+            <h3 id="section1-2" class="title">1.2 ChatGPT Plus 升级服务</h3>
+            <p class="desc" style="margin-top: 20px">
+              ChatGPT已上线一年半，Plus 相关权益完备：2023/10/13开始，
+              语音对接、AI 绘画 Dall-E 3 已经全部开放给 ChatGPT Plus 会员了！
+            </p>
+            <div class="bg-box" style="margin: 20px 0">
+              <p class="desc">
+                <strong>现在买，正当时。</strong>如果你已经了解了 Plus
+                会员来用上GPT-4 ，
+              </p>
+              <p class="desc" style="margin-top: 20px">我提供两个服务>></p>
+              <p class="yellow" style="margin-top: 20px">
+                1）ChatGPT Plus 成品号 –
+                无需你注册，拿来登录就用【被封包补】￥360
+              </p>
+              <p class="desc" style="margin-top: 10px">
+                简介：「成品号新号，全程代办交付 360
+                元。一步到位，我给你代办一个新号并升级为
+                Plus。正规充值，稳定好用——海外专线网络开始注册微软outlook邮箱、注册ChatGPT、正规认证的美国银行卡升级PLUS，包售后30天。——
+                GPT和邮箱账号都是你的，可修改密码」
+              </p>
+              <p class="yellow" style="margin-top: 20px">
+                2）ChatGPT Plus 会员代充代升级 GPT-4 正规充值 【被封包补】￥260
+              </p>
+              <p class="desc" style="margin-top: 10px">
+                简介：代充代升级 260 元，在你当前的账号上升级。
+              </p>
+              <p class="desc" style="margin-top: 10px">
+                注：在本店这里代办或充值，老客户下月续费时价格都有优惠，只需 230
+                元，半年起更优惠。
+              </p>
+            </div>
+          </div>
+
+          <p class="desc">
+            一个核心问题：<span class="yellow"
+              >为什么本站价格比官网要高呢？</span
+            >
+          </p>
+
+          <ul class="list">
+            <li>
+              <strong>确定性</strong>
+              :一定能办好，保障30天会员使用期
+            </li>
+            <li>
+              <strong>方便快捷</strong
+              >:10分钟办好;支持长期续费，不需要每月一续。
+            </li>
+            <li><strong>额外成本</strong>:开卡、入金等各个流程的手续费;</li>
+            <li>
+              <strong>售后保障</strong
+              >:被封包补，补差价重新办或按日计费退款都可以。
+            </li>
+            <li><strong>稍微赚点</strong>:做服务需要收益支持;</li>
+            <li>
+              <strong>陪伴价值</strong
+              >:关于ChatGPT和AI相关内容，咱们可以多做交流。你得到了一个相关技术专家。
+            </li>
+          </ul>
+
+          <p class="desc" style="margin-top: 20px">
+            本站正规代充值：价格构成为官方费用 20 刀+借记卡开卡费10
+            刀+充值手续费+人民币转美金的汇率费+人工费，其实就赚点人工费。还有承担被封号补办
+            Plus 的售后成本。
+          </p>
+
+          <p class="desc" style="margin-top: 20px">
+            下面通过
+            <strong>售前、售中、售后</strong>，三个部分详细解读。
+          </p>
+          <p class="desc" style="margin-top: 20px">
+            当然文章比较长，建议有耐心看完，如果你觉得，不管了，我先买一个，有问题就问站长，也行吧~
+          </p>
+        </section>
+
+        <!-- 售前介绍 -->
+        <section id="section2">
+          <h3 class="big-title" style="margin-top: 40px">
+            二、ChatGPT Plus 售前介绍
+          </h3>
+          <h3 id="section2-1" class="title" style="margin-top: 20px">
+            2.1 ChatGPT Plus产品权益介绍
+          </h3>
+          <p class="desc">
+            ChatGPT Plus 产品介绍及权益，ChatGPT Plus
+            是官方的付费会员服务，升级后可以享受强大的 GPT-4。官方订阅费用为每月
+            20 美元，订阅者将享有一系列的权益：
+          </p>
+          <el-image
+            src="https://chatgpt123.com/wp-content/uploads/2024/03/GPT-4-0329-1.jpg"
+            style="margin: 20px 0"
+          ></el-image>
+          <ul class="list">
+            <li>
+              <strong>GPT-4模型</strong>
+              ：质量比3.5高出数倍！自带联网查询实时数据，自动切换智能回答（所谓多模态），理解力很强
+            </li>
+            <li>
+              <strong>AI 绘画</strong>：即Dall-E3 模型）：直接向 GPT
+              提问画画，对话式改进画画
+            </li>
+            <li>
+              <strong>GPT-4V</strong>：即上传图片给 GPT
+              分析解读，结合绘画出图能力，提示生产力
+            </li>
+            <li>
+              <strong>GPT Voice语音对话</strong>：Voice 聊天功能，需要官方版
+              ChatGPT APP。请官方应用商店下载（美区苹果 iOS、安卓版则是 Google
+              Play，本站有教程，并会辅导付费客户搞定并下载 APP）
+            </li>
+            <li>
+              <strong>GPTs</strong
+              >：创建或使用他人的AI对话机器人。（替代了原有的 Plugins
+              插件功能，插件已下线）
+            </li>
+            <li><strong>GPTs 社区</strong>：可以使用更多优秀有趣的AI助手</li>
+            <li>
+              <strong>Sora文本生成视频</strong
+              >：暂未上线，预计优先给Plus会员覆盖使用，我也很期待~
+            </li>
+          </ul>
+          <p class="desc">
+            …… 太多优势功能，<strong
+              >你确认想要，我就能办，立即下单 Plus 会员</strong
+            >
+          </p>
+          <h3 id="section2-2" class="title" style="margin-top: 20px">
+            2.2 ChatGPT Plus 适合什么用户？
+          </h3>
+          <p class="desc">
+            结合本文一开始的表达，以下几类客户比较典型（不要求对号入座，我就买一个月体验下不行吗~~）：
+          </p>
+          <ul class="list">
+            <li>娱乐体验</li>
+            <li>工作需要</li>
+            <li>专业需求：对内容质量要求高，比如科研论文。</li>
+            <li>特色功能：语音对接、AI 绘画、GPTs 应用商店</li>
+            <li>......</li>
+          </ul>
+          <p class="desc">
+            当然看完上面资料，不确定你是买普通账号还是 Plus 会员，请看这个图片
+          </p>
+          <el-image
+            src="https://chatgpt123.com/wp-content/uploads/2024/03/WX20240318-231400@2x-1.png"
+            style="margin: 20px 0"
+          ></el-image>
+          <h3 id="section2-3" class="title" style="margin-top: 20px">
+            2.3 ChatGPT Plus 升级困难
+          </h3>
+          <p class="desc">
+            ChatGPT 升级的困难是什么？国人没有海外信用卡，OpenAI
+            对于信用卡的风控非常严格（很变态），对于支付环境都有很高的要求。当你在支付的时候，往往是没法支付成功的。
+          </p>
+          <p class="desc">常见报错内容：</p>
+          <ul class="list">
+            <li>Your card has been declined.</li>
+            <li>Your card does not support this type of purchase.</li>
+            <li>......</li>
+          </ul>
+          <el-image
+            src="https://chatgpt123.com/wp-content/uploads/2024/03/ChatGPT-Plus-payment-error-1.png"
+            style="width: 400px; margin: 20px 0"
+          ></el-image>
+          <p class="desc">
+            于是本站迎合市场需求，推出了两种服务：<span class="yellow"
+              >1）ChatGPT Plus 成品号出售 2） Plus代充代升级。</span
+            >
+          </p>
+        </section>
+
+        <!-- 售中介绍 -->
+        <section id="section3">
+          <h3 class="big-title" style="margin-top: 40px">
+            三、ChatGPT Plus 售前介绍三、ChatGPT Plus 售中介绍
+          </h3>
+          <h3 id="section3-1" class="title" style="margin-top: 20px">
+            3.1 ChatGPT Plus成品号购买
+          </h3>
+          <p class="desc">
+            这个商品适合不需要你自己有账号，你想买来就能用的 ChatGPT Plus 账号。
+            本商品，手工注册，安全有保障。你下单后，我将为你手动注册并充值 –
+            包售后（用不满 1 个月按时间退钱~）
+          </p>
+          <p class="desc" style="margin-top: 20px">
+            费用是 360 元，点击查看与购买>，包下号，为你全程交付。后续续费时只需
+            <span class="yellow">230 元/月</span>，随用随续，按月订阅的。
+          </p>
+          <p class="desc" style="margin-top: 20px">
+            欢迎联系林兄我微信：3056978。（有问题可答疑）。ChatGPT
+            Plus，用过的人都说值好吗~
+          </p>
+          <p class="desc" style="margin-top: 20px">
+            交易流程：下单后，加我个人微信，我即刻代办。
+          </p>
+          <el-image
+            src="https://chatgpt123.com/wp-content/uploads/2024/03/GPTPlus-成品号代办-1.jpg"
+            style="width: 300px; margin: 20px 0"
+          ></el-image>
+          <h3 id="section3-1" class="title" style="margin-top: 20px">
+            3.2 ChatGPT Plus 代充升级
+          </h3>
+          <p class="desc" style="margin-top: 20px">
+            ChatGPT Plus 代升级服务首次升级 Plus 需：<span class="yellow"
+              >260 元</span
+            >。后续续费 Plus：<span class="yellow">230 元</span>。
+          </p>
+          <p class="desc" style="margin-top: 20px">
+            ChatGPT Plus 会员的代充、代付、代升级流程如下：
+          </p>
+          <p class="desc" style="margin-top: 20px">
+            1）不需要账号密码，点击左下角的升级，在弹窗里点击升级
+          </p>
+          <el-image
+            src="https://chatgpt123.com/wp-content/uploads/2023/12/如何代充-ChatGPT-Plus，即如何获得付款链接-2.jpg"
+            style="margin: 20px 0"
+          ></el-image>
+          <p class="desc" style="margin-top: 20px">
+            网址是 https://pay.openai.com 开头的一长串网址。<strong
+              style="text-decoration: underline"
+              >不需要你的账号密码交给我，安全且保护隐私。</strong
+            >
+          </p>
+          <p class="desc" style="margin-top: 20px">注意两个细节：</p>
+          <ul class="list">
+            <li class="yellow">
+              生成链接时，请勿在付款页面填写卡信息和地址信息，由客服代购时填写；
+            </li>
+            <li class="yellow">
+              复制链接后，请勿退出或者刷新网页，将造成链接失效，需重新生成新链接。
+            </li>
+          </ul>
+        </section>
+
+        <!-- 售后保障 -->
+        <section id="section4">
+          <h3 class="big-title" style="margin-top: 40px">
+            四、ChatGPT Plus 售后保障
+          </h3>
+          <h3 id="section4-1" class="title" style="margin-top: 20px">
+            4.1 GPT Plus 会员被封包补
+          </h3>
+          <p class="desc">本店Plus会员的售后政策是这样的：</p>
+          <div class="bg-box" style="margin: 20px 0">
+            <p class="desc">
+              被封包补的是说，按日计费，需补差价新办理一个月会员，或退款剩余天数的费用。比如一个月会员，按续费价格230，用了15天，可以补230/2=115元补办一个月的会员，或者退款115元都支持。
+            </p>
+          </div>
+          <p class="desc">这是我能够给到的售后保障，其他客户也很认可的方案。</p>
+          <h3 id="section4-2" class="title" style="margin-top: 20px">
+            4.2 ChatGPT 账号防封技巧
+          </h3>
+          <p class="desc" style="margin-top: 20px">
+            ChatGPT Plus 账号为啥被封？可以参考这篇 ChatGPT
+            封号的原因是什么？如何避免？
+          </p>
+          <p class="desc" style="margin-top: 20px">
+            的确有正常使用 Plus 账号而被封的情况。需要注意的是：
+          </p>
+          <ul class="list">
+            <li>
+              <strong>稳定节点</strong> ——
+              避免经常更换节点，需要用稳定的、延时低的付费节点。勿用免费节点（往往被污染过）。
+            </li>
+            <li>
+              <strong>非批量注册的号</strong> ——
+              那种机器注册的号容易被封，我可以给你手工注册并充值为 Plus
+              账号，如上所述。
+            </li>
+            <li>
+              <strong>合规使用</strong> ——
+              尽量避免共享。——即便是要共享，但也要求用同一个节点。
+            </li>
+            <li>
+              <strong>与 API 调用的账号分开来</strong> —— Plus
+              账号主要用于对话，而 API 调用的账号选用其他，否则可能因为 API
+              调用不合规或超出使用额度而产生了费用拖欠，因为 API
+              调用被封账号也殃及了 Plus 的会员权益。
+            </li>
+          </ul>
+          <h3 id="section4-3" class="title" style="margin-top: 20px">
+            4.3 用户好评
+          </h3>
+          <p class="desc" style="margin-top: 20px">
+            如下图是一些在我这边升级为 ChatGPT Plus
+            的用户，升级的记录，目前案例太多了，欢迎加我朋友圈围观哈哈哈。
+            （微信号：3056978）
+          </p>
+          <el-image
+            src="https://chatgpt123.com/wp-content/uploads/2023/08/WX20230816-161503@2x-1.png"
+            style="margin: 20px 0"
+          ></el-image>
+          <h3 id="section4-4" class="title" style="margin-top: 20px">
+            4.4 升级后续
+          </h3>
+          <p class="desc" style="margin-top: 20px">
+            升级完 ChatGPT Plus 后需要做什么？
+          </p>
+          <p class="desc" style="margin-top: 20px">
+            1）密码维护：通过链接代付的，不需要修改密码。如果给了账号密码给其他人，那可以考虑修改。
+          </p>
+          <p class="desc" style="margin-top: 20px">
+            2023 年 10 月 23 日更新：新增了 Bing
+            的联网能力，目前该功能是默认合并在 GPT-4 的模型里。
+          </p>
+          <el-image
+            src="https://chatgpt123.com/wp-content/uploads/2023/08/ChatGPT-Plus-Beta-Features-1.jpg"
+            style="margin: 20px 0"
+          ></el-image>
+          <p class="desc" style="margin-top: 20px">GPT-4 的API 怎么得到呢？</p>
+          <p class="desc" style="margin-top: 20px">
+            科普的是，ChatGPT 和 OpenAI 的API 是两个体系。而 GPT4 的 API
+            得到也不难 —— 即你的 OpenAI账号重新API，即可享受 GPT-4 的
+            API。目前已经全面开放了给 API 付费用户。
+          </p>
+          <div class="bg-box" style="margin: 20px 0">
+            <p class="desc api-key">
+              OpenAI API 的终极解决方案，如何绑卡充值 Key、如何不限速调用？
+            </p>
+          </div>
+        </section>
+
+        <!-- 普通账号与plus区别 -->
+        <section id="section5">
+          <h3 class="big-title" style="margin-top: 40px">
+            五、ChatGPT 普通账号和 Plus 会员差别
+          </h3>
+          <p class="desc" style="margin-top: 20px">
+            用一个表格一目了然的对比下：
+          </p>
+          <el-table
+            :data="tableData"
+            border
+            style="width: 100%; margin-top: 20px"
+          >
+            <el-table-column prop="target" label="对比项" />
+            <el-table-column prop="normal" label="ChatGPT 普通账号" />
+            <el-table-column
+              prop="plus"
+              label="ChatGPT Plus 账号
+"
+            />
+          </el-table>
+          <p class="desc" style="margin-top: 20px">
+            更多请看 ChatGPT Plus 专业购买<span class="yellow">
+              https://www.buychatgptplus.com</span
+            >
+          </p>
+          <div class="bg-box" style="margin: 20px 0">
+            <p class="desc">
+              注：ChatGPT 的 Dall-E 3 和 GPT-4V，已经全面开放给 Plus 了！以及
+              Voice
+              功能在内测。但新号往往直接具备开通了。欢迎加我微信了解（3056978），并从朋友圈围观看到更多及时信息。
+            </p>
+          </div>
+        </section>
+
+        <!-- 如何续费会员 -->
+        <section id="section6">
+          <h3 class="big-title" style="margin-top: 40px">
+            六、ChatGPT Plus 如何续费会员？
+          </h3>
+          <!-- <h3 id="section4-1" class="title" style="margin-top: 20px">
+            4.1 GPT Plus 会员被封包补
+          </h3> -->
+          <p class="desc" style="margin-top: 20px">
+            后续续费，我一样能够提供长期服务保障。请看文章 >>
+          </p>
+          <p class="desc yellow" style="margin-top: 20px">
+            https://chatgpt123.com/chatgpt-plus-renew
+          </p>
+          <p class="desc" style="margin-top: 20px">
+            但一般是找人代充，我这里提供续费服务。需要客户给两个链接，
+          </p>
+          <p class="desc" style="margin-top: 20px">
+            1）订阅管理页的网址 2）订阅支付页网址，如图所示：
+          </p>
+          <p class="desc" style="margin-top: 20px">
+            ChatGPT Plus 续费首先点击左下角的 Renew Plus
+          </p>
+          <el-image
+            src="https://chatgpt123.com/wp-content/uploads/2023/09/RenewPlus.jpg"
+            style="margin: 20px 0; width: 600px"
+          ></el-image>
+          <p class="desc" style="margin-top: 20px">
+            ChatGPT Plus 续费，然后点击弹窗里的 支付和管理订阅
+          </p>
+          <el-image
+            src="https://chatgpt123.com/wp-content/uploads/2023/09/Upgrade2Plus.jpg"
+            style="margin: 20px 0; width: 600px"
+          ></el-image>
+          <p class="desc" style="margin-top: 20px">
+            这两个地址都需要。发送给我后，接下来就可以交给我了！只需要 10 分钟。
+          </p>
+          <p class="desc" style="margin-top: 20px">ChatGPT Plus 续费的价格</p>
+          <p class="desc" style="margin-top: 20px">
+            包你续费成功！ 老客户只需要 230 元/月。
+            新客户和成品号价格因市场环境和官方正常有波动，请点击查看最新价格。
+          </p>
+          <p class="desc" style="margin-top: 20px">
+            欢迎加我微信：3056978，帮助您完成 Plus 会员的成品号、新升级或续费。
+          </p>
+          <el-image
+            src="https://chatgpt123.com/wp-content/uploads/2023/08/AI-林兄.jpg"
+            style="margin: 20px 0; width: 300px"
+          ></el-image>
+        </section>
       </div>
 
-      <el-anchor>
+      <el-anchor class="small-nav">
         <el-anchor-link
           href="#section1"
           title="1. ChatGPT Plus 升级服务介绍"
@@ -97,23 +557,121 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
-// import anchorLocale from "../../.vitepress/i18n/component/anchor.json";
-// import { useLang } from "~/composables/lang";
-
-// const lang = useLang();
-// const locale = computed(() => anchorLocale[lang.value]);
-
-const scrollToSection = (sectionId) => {
+const scrollToSection = (sectionId: string) => {
   const section = document.getElementById(sectionId);
   if (section) {
     section.scrollIntoView({ behavior: "smooth" });
   }
 };
+
+const tableData = [
+  {
+    target: "模型",
+    normal: "GPT-3.5",
+    plus: "GPT-4独享、GPT 多模态、GPT-3.5",
+  },
+  {
+    target: "模型升级",
+    normal: "常规升级",
+    plus: "优享最新",
+  },
+  {
+    target: "响应速度",
+    normal: "速度很快，内容质量一般",
+    plus: "速度一般，内容质量高",
+  },
+  {
+    target: "Bing 联网模型",
+    normal: "无",
+    plus: "速度一般，内容质量高，有！不受限于 2021 年的数据了！",
+  },
+  {
+    target: "高级数据分析",
+    normal: "无",
+    plus: "有！ Python 图表数据可视化能力",
+  },
+  {
+    target: "代码增强能力",
+    normal: "无",
+    plus: "有！可上传文件夹，进行代码解析",
+  },
+  {
+    target: "第三方插件商店",
+    normal: "无",
+    plus: "有！丰富的插件市场",
+  },
+  {
+    target: "Dall-E 3 （AI 绘画）",
+    normal: "无",
+    plus: "有！丰富的插件市场有！效果媲美 Midjourney，更易用",
+  },
+  {
+    target: "GPT-4V （识图分析）",
+    normal: "无",
+    plus: "有！可让 ChatGPT 识别图片并分析",
+  },
+  {
+    target: "GPT-4V （识图分析）GPTs 多模态",
+    normal: "无",
+    plus: "有！可根据内容自动切换更适合回答的模型，比如经典GPT-4、AI绘画、高级数据分析、写代码模式等",
+  },
+  {
+    target: "GPTs 应用机器人商店",
+    normal: "无",
+    plus: "有！可自建或使用他人创建的 GPTs Bot 🤖",
+  },
+  {
+    target: "ChatGPT Voice",
+    normal: "有！APP 端可开启语音对聊对话模式",
+    plus: "有！APP 端可开启语音对聊对话模式",
+  },
+  {
+    target: "价格",
+    normal: "免费注册但有门槛",
+    plus: "找我升级（微信 3056978），稳定可靠包售后",
+  },
+];
 </script>
 <style lang="scss" scoped>
 .content {
   display: flex;
   justify-content: space-between;
+}
+.big-title {
+  font-size: 30px;
+}
+.title {
+  margin: 10px 0;
+}
+.box {
+  margin-bottom: 40px;
+}
+.list {
+  font-size: 14px;
+  margin: 20px 0;
+  padding-left: 40px;
+  list-style: circle;
+  li {
+    line-height: 30px;
+  }
+}
+.bg-box {
+  background: #ecf5ff;
+  padding: 20px;
+  border-radius: 8px;
+}
+.main-box {
+  padding-right: 350px;
+}
+.small-nav {
+  position: fixed;
+  right: 0;
+  top: 100px;
+}
+.api-key {
+  cursor: pointer;
+  &:hover {
+    color: rgb(223, 20, 20);
+  }
 }
 </style>
